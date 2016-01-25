6ab1f48f5e0bda5fcff3e3e97bfed05b4c3cfda5
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-01-24 20:03:50 -0800

    port.

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ function handleRequest(request, response){
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(port, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
