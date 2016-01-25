336baffc40884aeca1708db3b7324e0a38934e85
Jeff Jenkins <jenkinsjeffrey@gmail.com>
2016-01-24 20:17:32 -0800

    adding confirmation.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ ref.on("child_added", function(snapshot, prevChildKey) {
 	const email = newPost.emailNotification
 	const from = 'noreply@fireform.org'
 	const subject = 'A new post was made to the form ' + newPost.fireFormRepo
-	var text = 'A post faw made to '+ newPost.fireFormRepo + ' with the data:\r\n'
+	var text = 'A post was made to '+ newPost.fireFormRepo + ' with the data:\r\n'
 	snapshot.child('form').forEach(function(child){
 		var string =  (child.val().name +' : '+child.val().value + "\r\n");
 		text += string
@@ -45,16 +45,18 @@ refConfirm.on("child_added", function(snapshot, prevChildKey) {
 	var newPost = snapshot.val();
   
 	const email = newPost.emailConfirmation
-	const from = 'noreply@fireform.org'
-	const subject = 'Thanks for your submission.'
-	var text = 'Success'
+	const from =  newPost.emailConfirmationFrom
+	const subject = newPost.emailConfirmationSubject
+	const html = newPost.emailConfirmationBodyHTML
+	const text = newPost.emailConfirmationBodyText
 
 
 	sendgrid.send({
 	  to:       email,
 	  from:     from,
 	  subject:  subject,
-	  text:     text
+	  text:     text,
+	  html: 	html
 	}, function(err, json) {
 	  if (err) { return console.error(err); }
 	  console.log(json);
@@ -73,13 +75,13 @@ ref.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
   }
 });
 
-// refConfirm.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
-//   if (error) {
-//     console.log("Login Failed!", error);
-//   } else {
-//     console.log("Login Succeeded!", authData);
-//   }
-// });
+refConfirm.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!", authData);
+  }
+});
 
 
 //Lets require/import the HTTP module
